cecb27804b05b35201284c56e85c03e969474156
valerieefk <valerie.fleischauer@sydortechnologies.com>
2022-08-01 16:41:10 -0400

    Update openimage.py

--- openimage.py
+++ openimage.py
@@ -1,34 +1,53 @@
-#Created VEFK 08/01/22
-#Program to load in image files
+# Created vefk 08/01/22
+# Program to load in image files
 
 from datetime import datetime
+from tabnanny import filename_only
 from pytz import timezone
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import griddata
 import os
 
+# where is your data? 
+path = "C:/data/TXC_NSLS2_JUL2022/diamond1_images/"
+filename = "ah.raw"
+file = path + filename
+print(file)
+
+# TXC file type
 dt = np.dtype('int32')
-data = np.fromfile("/Users/benjaminmartin/Documents/GitHub/ScratchPad/areascan_6fps_hi_100umpin_1p5Al_1.raw", dtype = dt)
+data = np.fromfile(file, dtype = dt)
 dSize = np.size(data)
 imageTotal = int(dSize / 1024)
+
+# some information about the image stack 
 print ('Total # images')
 print(imageTotal)
-print ("the total average of all data")
-totalframeAvg = np.average(data)
+print ("the total average of all data") 
+totalframeAvg = np.average(data)   #this prints the average value of every unit in the array
 print (totalframeAvg)
 frameAvg = np.array(1)
 print(range(imageTotal))
-for i in range(imageTotal):
-    imStart = (i-1)*1024
-    imEnd = i * 1024
-    image = data[imStart:imEnd]
-    #data2d = image.reshape(32,32)
-    iAvg = (np.average(image))
-    #print(np.average(image))
-    frameAvg = np.append(frameAvg,iAvg) 
-#print (data2d)
-data2d = np.resize(frameAvg, (50,50))
-plt.contourf(data2d)
-plt.show()
-#print(frameAvg)
+
+# which image do you want to display?
+i = 5 
+imStart = (i-1)*1024
+imEnd = i * 1024
+
+# convert the data into a 2D array to display 
+image = data[imStart:imEnd]
+data2d = image.reshape(32,32)
+flipped = np.flip(data2d, 0) # TXC data opens upside down without this step in this import method
+
+# dispaly that sweet sweet data 
+plt.contourf(flipped)   # what type of plot? also applies the flip.
+plt.colorbar(label="ADU", orientation="vertical")   # intensity scale
+plt.xlabel('horizontal pixel number')
+plt.ylabel('vertical pixel number')
+plt.title(filename)
+
+
+plt.show()   # brings up the plot window 
+
+
